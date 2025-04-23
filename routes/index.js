328e2444cf78c35be58d4736cd545747d5d42383
Kevin <kevinkock@MacBook-Air-de-Kevin.local>
2025-04-23 16:59:35 +0200

    mymoviz-part5-backend

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/movies', (req, res) => {
-  fetch("https://api.themoviedb.org/3/discover/movie?api_key=4a6228566b5df9d31d0ff69f8242f9ed")
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.OWM_API_KEY}`)
   .then((response) => response.json())
   .then((data) => res.json({ movies: data.results }))
    
